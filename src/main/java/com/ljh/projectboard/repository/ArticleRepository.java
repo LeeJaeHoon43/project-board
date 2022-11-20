1710d425e27ee8b4d4e5844db2cae3e692c2dac1
LeeJaeHoon43 <ljw160791@gmail.com>
2022-11-20 23:21:09 +0900

    #15 - 서비스 로직 테스트 코드 업데이트 및 DTO 재설계, 쿼리 메소드 작성
    게시글 서비스 로직의 테스트와 골격 잡기.
    댓글 서비스 로직의 테스트와 골격 잡기.
    
    다음의 기능을 테스트로 표현
    
    * 게시글 검색하면 게시글 리스트 반환
    * 게시글을 ID로 조회
    * 게시글 생성
    * 게시글 수정
    * 게시글 삭제
    
    테스트 코드를 작성하고 연관된 DTO 를 재설계함.
    테스트가 필요로 하는 repository 쿼리 메소드도 작성.

--- src/main/java/com/ljh/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/ljh/projectboard/repository/ArticleRepository.java
@@ -1,18 +1,24 @@
 package com.ljh.projectboard.repository;
 
 import com.ljh.projectboard.domain.Article;
+import com.ljh.projectboard.domain.ArticleComment;
 import com.ljh.projectboard.domain.QArticle;
 import com.querydsl.core.types.dsl.DateTimeExpression;
 import com.querydsl.core.types.dsl.StringExpression;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import java.util.List;
 
 @RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long>,
         QuerydslPredicateExecutor<Article>, QuerydslBinderCustomizer<QArticle> {
+
+    Page<Article> findByTitle(String title, Pageable pageable);
 
     @Override
     default void customize(QuerydslBindings bindings, QArticle root){
